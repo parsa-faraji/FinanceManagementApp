0b66bc94309aa596142b37387bdfc608b84a8f0b
parsa-faraji <parsafaraji8213@gmail.com>
2024-11-15 19:29:47 -0800

    Initial Commit

---
+++ FinanceManagementApp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FinanceManagementApp
+//
+//  Created by Parsa Faraji on 11/15/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
